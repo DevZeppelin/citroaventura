0329693466ab1d7cbd0bd6dc2f6c882fa8e2a5a3
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2021-12-06 23:13:06 -0300

    seeing if second fixed background now found, but before in production not work

--- components/SectionBackgroundFixed2.js
+++ components/SectionBackgroundFixed2.js
@@ -3,16 +3,16 @@ import Fade from "react-reveal/Fade";
 const SectionBackgroundFixed2 = () => {
   return (
     <section>
-      <div className="p-8 md:p-32 py-16 font-bold text-center bg-cover bg-love-m md:bg-love bg-center bg-fixed flex md:grid md:grid-cols-2 h-screen ">
+      <div className="p-8 md:p-32 py-16 font-bold text-center bg-cover bg-love-m md:bg-love bg-center bg-fixed md:grid md:grid-cols-2 h-screen ">
         <div></div>
         <div className="space-y-6">
+            <div className="text-gray-800 space-y-3 text-xl textLight font-bold flex align-text-top ">
           <Fade left>
-            <div className="text-gray-800 space-y-3 text-xl textLight font-bold justify-start">
               <p>
               ¿Estás listo para ver hasta donde es capáz de llegar la comunidad Citronera unida sin fronteras? 
               </p>
-            </div>
           </Fade>
+            </div>
         </div>
       </div>
       <style jsx>{`
